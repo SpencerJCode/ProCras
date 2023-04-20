35a64ddaf8116a63ce899103e3637bd2ac993bee
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-04-20 14:31:36 -0500

    initial commit

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const dbName = process.env.DB;
+const username = process.env.ATLAS_USERNAME;
+const pw = process.env.ATLAS_PASSWORD;
+const uri = `mongodb+srv://${username}:${pw}@cluster0.cycjgio.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+mongoose.connect(uri)
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
