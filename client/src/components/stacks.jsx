7788b6d50c52e054ffb4b9f4487108ee2374ea70
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-04-25 06:20:02 -0500

    buttons wip

--- client/src/components/stacks.jsx
+++ client/src/components/stacks.jsx
@@ -15,11 +15,11 @@ const createHoverEffect = () => {
 
 // creating a new stacks should also set that stack as the highlighted stack
     return (
-        <div className="d-flex align-items-center gap-5 mt-3 col-11 m-auto">
+        <div className="d-flex align-items-center mt-3 m-auto">
             {stacks.map((stack, i) => {
                 if (!(stack.studySession)) {
                 return (
-                <div className=" d-flex flex-column align-items-center col-3 m-auto">
+                <div className=" d-flex flex-column align-items-center m-auto">
                     <div onClick = {(e) => {highlightedStack(stack._id); showButtons(stack._id) }} className= "hover-deck-stack hover-effect">
                         <div className = "hover-deck-stack-content">
                             <h1 className="deck-stack-title">{stack.stackName}</h1>
